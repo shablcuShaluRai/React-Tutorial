8a559d72512c10855a3f908871ce121add2b5bf5
shalurai <shaluraics22@gmail.com>
2017-07-24 15:32:56 +0900

    stateless component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
 
 import './App.css';
+// stateless Component example
 
 class App extends Component {
   render() {
     return (
       // div is jsx code
     <div>
-    {/*nested element*/}
-    {/* one or more element wrap with one container class.
-      * here div is container class and h1,p,h2 is the element .
-       */}
-    <div className = "App">
-    <h1 className = "rcorners1"> Header </h1>
-<p> this is content</p>
-<h2> content </h2>
-{/*javascript expression can be used inside jsx*/}
+{/*Header  and Content  component called inside the jsx */}
+    <Header/>
+   <Content/>
 
-<h1>{1+2+3+4}</h1>
-</div>
     </div>
     );
   }
 }
+
+//it is also a component
+class Header extends Component {
+  render(){
+    return(
+      <div className = "App">
+      <h1 className = "rcorners1"> Header </h1>
+</div>
+
+    )
+  }
+
+  }
+  class Content extends Component {
+     render(){
+       return(
+         <div>
+         <p> This is content</p>
+         <h2> content </h2>
+       </div>
+     )
+     }
+  }
+
 
 export default App;
