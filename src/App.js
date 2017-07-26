47e5a8b1286d8e554a73312c05c82413ebfefcc5
shalurai <shaluraics22@gmail.com>
2017-07-26 10:32:33 +0900

    use of setState

--- src/App.js
+++ src/App.js
@@ -4,52 +4,30 @@ import './App.css';
 
 
 class App extends Component {
+  // For data that is going to change, we have to use state.
+state = {
+  data:[]
+}
+setStateHandler = this.setStateHandler.bind(this);
+// it is function, which called on  onClick event.
+setStateHandler(){
+  var item = "Shalu..."
+  var marray = this.state.data;
+  //The push() method adds new items to the end of an array, and returns the new length.
+  //it adds the data in  array .
+  marray.push(item);
+  //it updates the state of data.
+  //here, data contains the new length of the marray
+  this.setState({data:marray })
+}
+
 render(){
   return(
     <div>
-           <h3>Array: {this.props.propArray}</h3>
-           <h3>Bool: {this.props.propBool ? "True..." : "False..."}</h3>
-           <h3>Func: {this.props.propFunc(3)}</h3>
-           <h3>Number: {this.props.propNumber}</h3>
-           <h3>String: {this.props.propString}</h3>
-           <h3>Object: {this.props.propObject.objectName1}</h3>
-           <h3>Object: {this.props.propObject.objectName2}</h3>
-           <h3>Object: {this.props.propObject.objectName3}</h3>
-        </div>
-     );
-
-}
-}
-
-// we can declare that a prop is a specific JS primitive. By default, these
-  // are all optional.
-//  If I’m using a component written by another developer I have to figure out what props that component want, what’s required and also the correct type.
-// React has a solution for this and its called propTypes. PropTypes defines type and which props are required.
-// This benefits the future all developer using  component in two ways:
-
-//1. we can easily open up a component and check which props are required and what type they should be.
-//2. When things get messed up React will give  an awesome error message in the console, 
-//saying which props is wrong/missing plus the render method that caused the problem.
-
-App.propTypes = {
-   propArray: React.PropTypes.array.isRequired,
-   propBool: React.PropTypes.bool.isRequired,
-   propFunc: React.PropTypes.func,
-   propNumber: React.PropTypes.number,
-   propString: React.PropTypes.string,
-   propObject: React.PropTypes.object
-}
-App.defaultProps = {
-   propArray: [1,2,3,4,5],
-   propBool: true,
-   propFunc: function(e){return e},
-   propNumber: 1,
-   propString: "String value...",
-
-   propObject: {
-      objectName1:"objectValue1",
-      objectName2: "objectValue2",
-      objectName3: "objectValue3"
+    <button onClick = {this.setStateHandler}>SET STATE</button>
+    <h4>{this.state.data}</h4>
+    </div>
+  );
    }
 }
 
