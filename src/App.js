75c1416899f47931ef5dce6673222cab66e1c61e
shalurai <shaluraics22@gmail.com>
2017-07-28 17:57:27 +0900

    React Router Example

--- src/App.js
+++ src/App.js
@@ -1,55 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react'
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
-import './App.css';
-
-
-class App extends Component {
-state={
-  data:[ {
-    'id':1,
-    'compont':'first'
-},
-{
-  'id':2 ,
-  'compont' : 'second'
-},
-{
-  'id' :3 ,
-  'compont' : 'third'
-}
-]
-}
-
-render(){
-  return (
+// React router using arrow function
+const BasicExample = () => (
+  //The <Route> component is the main building block of React Router.
+  // Anywhere that we  want to only render something if it matches the location’s pathname,
+  // then we should create a <Route> element.
+  <Router>
     <div>
-    {/*React keys are useful when working with dynamically created components or when lists are altered by users*/}
-    {/*setting the key value will keep our component uniquely */}
-    {this.state.data.map((component,index) =>
-      <Content
-      key = {index}
-      components = {component}
-      />
+      <ul>
+      //Finally, our application needs a way to navigate between pages.
+      // If we were to create links using anchor elements, clicking on them would cause the whole page to reload.
+      // React Router provides a <Link> component to prevent that from happening.
+      // When clicking a <Link>, the URL will be updated and the rendered content will change without reloading the page.
 
-    )}
-</div>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/topic">Topic</Link></li>
+      </ul>
 
-  )
-}
+      <hr/>
 
-}
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+        <Route path="/topic" component={Topic}/>
+
+    </div>
+  </Router>
+)
+
+const Home = () => (
+  <div>
+    <h2>Now!Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2> All About Link </h2>
+  </div>
+)
+const Topic = () => (  <div>
+  <h2>topic is here </h2>
+  </div>
+)
 
 
-class Content extends Component{
-  render(){
-    return(
-      <div>
-      <div>{this.props.components.compont}</div>
-      <div>{this.props.components.id}</div>
-</div>
-    )
-  }
-}
 
-export default App;
+export default BasicExample
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { HashRouter } from 'react-router-dom'
 import './index.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+// The <BrowserRouter> should be used when you have a server that will handle dynamic requests
+//(knows how to respond to any possible URI),
+// while the <HashRouter> should be used for static websites
+ReactDOM.render((<HashRouter>
+    <App/>
+   </HashRouter>), document.getElementById('root'));
 
 registerServiceWorker();
